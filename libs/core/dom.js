103aa3172739a504e3d808a95a6fc68b9f9ce5ea
DreamEcho100 <maze6572198@gmail.com>
2025-04-20 21:46:26 +0200

    Updates

--- libs/core/dom.js
+++ libs/core/dom.js
@@ -1,8 +1,8 @@
-import{buttonPrimaryClassName as r}from"../class-names.js";import{CleanUpManager as d}from"../cleanup.js";import{injectStylesheetLink as u,loadManyAssets as f}from"../dom.js";import"../math.js";async function g(t){const c=`app-${Math.random().toString(32)}`,l=`go-back-${c}`,a=new d;return t.stylesheetLink&&u(t.stylesheetLink,a),async e=>{function s(){e.handleGoPrevScreen?.(),a.cleanUp()}let o;if(t.assetsInfo){const[n,m]=await f(t.assetsInfo);if(e.appElem.innerHTML=`
+import{buttonPrimaryClassName as r}from"../class-names.js";import{CleanUpManager as d}from"../cleanup.js";import{injectStylesheetLink as u,loadManyAssets as f}from"../dom.js";import"../math.js";async function g(t){const c=`app-${Math.random().toString(32)}`,l=`go-back-${c}`,a=new d;return t.stylesheetLink&&u(t.stylesheetLink,a),async e=>{function s(){e.handleGoPrevScreen?.(),a.cleanUp()}let o;if(t.assetsInfo){e.appElem.innerHTML=`
 			<section class="flex justify-center items-center p-12 text-lg">
 			Loading assets...
 			</section>
-			`,n){console.error(n),e.appElem.innerHTML=`<section
+			`;const[n,m]=await f(t.assetsInfo);if(n){console.error(n),e.appElem.innerHTML=`<section
 				class="p-8 bg-slate-50 dark:bg-slate-900 w-full min-h-full text-slate-900 dark:text-slate-50 flex flex-col gap-4 max-w-full"
 			>
 				${e.handleGoPrevScreen?`<button id="${l}" class="${r}">Go Back</button><br /><br />`:""}
