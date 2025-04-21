103de4ef946389853d8afa2aa0ab9f25737c4e5c
DreamEcho100 <maze6572198@gmail.com>
2025-04-21 15:31:34 +0200

    Updates

--- libs/core/dom.js
+++ libs/core/dom.js
@@ -1,4 +1,4 @@
-import{buttonPrimaryClassName as r}from"../class-names.js";import{CleanUpManager as d}from"../cleanup.js";import{injectStylesheetLink as u,loadManyAssets as f}from"../dom.js";import"../math.js";async function g(t){const c=`app-${Math.random().toString(36).slice(2)}`,l=`${c}-go-back-button`,a=new d;return t.stylesheetLink&&u(t.stylesheetLink,a),async e=>{function s(){e.handleGoPrevScreen?.(),a.cleanUp()}let o;if(t.assetsInfo){e.appElem.innerHTML=`
+import{buttonPrimaryClassName as r}from"../class-names.js";import{CleanupManager as u}from"../cleanup.js";import{injectStylesheetLink as d,loadManyAssets as f}from"../dom.js";import"../math.js";async function g(t){const c=`app-${Math.random().toString(36).slice(2)}`,l=`${c}-go-back-button`,a=new u;return t.stylesheetLink&&d(t.stylesheetLink,a),async e=>{function s(){e.handleGoPrevScreen?.(),a.cleanup()}let o;if(t.assetsInfo){e.appElem.innerHTML=`
 			<main class="flex justify-center items-center p-12 text-lg">
 			Loading assets...
 			</main>
@@ -13,4 +13,4 @@ import{buttonPrimaryClassName as r}from"../class-names.js";import{CleanUpManager
 	>
 		${e.handleGoPrevScreen?`<button id="${l}" class="${r}">Go Back</button>`:""}
 		${n}
-	</main>`,a.registerEventListener({elem:document.getElementById(l),type:"click",listener:s,silent:!1})};return t.cb({assets:o,cleanUpManager:a,appId:c,goBackButtonId:l,goBack:s,createLayout:i})}}export{g as default};
+	</main>`,a.registerEventListener({elem:document.getElementById(l),type:"click",listener:s,silent:!1})};return t.cb({assets:o,cleanupManager:a,appId:c,goBackButtonId:l,goBack:s,createLayout:i})}}export{g as default};
